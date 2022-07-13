c690af7b86efe42435d9456e0a9050fa38bd7dd1
kimpv <kimpv@volio.vn>
2022-07-13 12:45:19 +0700

    More command

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node:16.0.0-alpine
 
+RUN echo "Create folder /srv/app"
+
 RUN mkdir /srv/app
 
 WORKDIR /pre/app
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
+
+echo "Copy resource from /pre/app to /srv/app"
+
 cp -r /pre/app /srv/app
+
+echo "cd to /srv/app"
 
 cd /srv/app
 
+ls
+
+echo "Installing"
+
 yarn install
 
+echo "Building"
+
 yarn build
+
+echo "Starting...!"
 
 yarn start
