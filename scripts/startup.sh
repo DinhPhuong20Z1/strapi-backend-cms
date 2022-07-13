08902f4bab7683392acb22ab5bb726582d8a09a3
kimpv <kimpv@volio.vn>
2022-07-13 12:47:32 +0700

    Build

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "Content of /pre/app"
+ls /pre/app
+
+echo "Content of /srv/app"
+ls /srv/app
+
 echo "Copy resource from /pre/app to /srv/app"
 
 cp -r /pre/app /srv/app
@@ -8,7 +14,7 @@ echo "cd to /srv/app"
 
 cd /srv/app
 
-ls
+ls /srv/app
 
 echo "Installing"
 
