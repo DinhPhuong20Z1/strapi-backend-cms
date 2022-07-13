2def7555b0c57fd67543aed54d7dc117ea0a19a7
kimpv <kimpv@volio.vn>
2022-07-13 13:44:32 +0700

    Rv some line in scirpt

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,29 +1,9 @@
 #!/bin/sh
 
-echo "Content of /pre/app"
-ls -la /pre/app
-
-echo "Content of /srv/app"
-ls -la /srv/app
-
-echo "Copy resource from /pre/app to /srv/app"
-
 cp -r /pre/app/* /srv/app
-
-echo "cd to /srv/app"
 
 cd /srv/app
 
-ls -la /srv/app
-
-echo "Installing"
-
 yarn install
-
-echo "Building"
-
 yarn build
-
-echo "Starting...!"
-
 yarn start
