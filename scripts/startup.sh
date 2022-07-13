1931162e6ec9fac54ef8a2021cefb88125dec1bd
kimpv <kimpv@volio.vn>
2022-07-13 13:00:52 +0700

    New cp content

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 echo "Content of /pre/app"
-ls /pre/app
+ls -la /pre/app
 
 echo "Content of /srv/app"
-ls /srv/app
+ls -la /srv/app
 
 echo "Copy resource from /pre/app to /srv/app"
 
-cp -r /pre/app /srv/app
+cp -r /pre/app/* /srv/app
 
 echo "cd to /srv/app"
 
 cd /srv/app
 
-ls /srv/app
+ls -la /srv/app
 
 echo "Installing"
 
