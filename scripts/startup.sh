c0b10ad527d1b48fddb478cb61bb195e91cfe5db
kimpv <kimpv@volio.vn>
2022-07-15 10:21:46 +0700

    yarn install

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-rm -rf /srv/app/build /srv/app/config /srv/app/node_modules
+rm -rf /srv/app/build /srv/app/config
 
 cp -rf /pre/app/* /srv/app
 
 diff /pre/app/package.json /srv/app/package.json
 
 cd /srv/app
+
+yarn install
 
 yarn develop
