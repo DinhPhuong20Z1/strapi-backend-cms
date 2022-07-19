ac48cbae113ea72a2bded7fa7331f78124e7a970
kimpv <kimpv@volio.vn>
2022-07-19 14:40:02 +0700

    Custom upload

--- extensions/upload/services/Upload.js
+++ extensions/upload/services/Upload.js
@@ -12,6 +12,8 @@ module.exports = {
       generateResponsiveFormats,
     } = strapi.plugins.upload.services["image-manipulation"];
 
+    console.log("Volio upload fileData.mime: ", fileData.mime);
+    console.log("Volio upload fileData.path: ", fileData.path);
     await strapi.plugins.upload.provider.upload(fileData);
 
     const base64 = await generateBase64(fileData);
@@ -21,24 +23,27 @@ module.exports = {
 
     const thumbnailFile = await generateThumbnail(fileData);
     if (thumbnailFile) {
+      console.log("Volio upload thumbnailFile.mime: ", thumbnailFile.mime);
+      console.log("Volio upload thumbnailFile.path: ", thumbnailFile.path);
+
       await strapi.plugins.upload.provider.upload(thumbnailFile);
       delete thumbnailFile.buffer;
       fileFormats["thumbnail"] = [thumbnailFile];
     }
 
     const formats = await generateResponsiveFormats(fileData);
-    if (Array.isArray(formats) && formats.length > 0) {
+    if (
+      Array.isArray(formats) &&
+      formats.length > 0 &&
+      format[0] !== undefined
+    ) {
       for (const format of formats) {
-        if (
-          !format ||
-          !(
-            Array.isArray(format) &&
-            format.length > 0 &&
-            format[0] !== undefined
-          )
-        )
-          continue;
+        if (!format || !(Array.isArray(format) && format.length > 0)) continue;
         for (const { key, file } of format) {
+          console.log("Volio upload key: ", key);
+          console.log("Volio upload file.mime: ", file.mime);
+          console.log("Volio upload file.path: ", file.path);
+
           await strapi.plugins.upload.provider.upload(file);
           delete file.buffer;
 
@@ -65,11 +70,16 @@ module.exports = {
     });
     return this.add(fileData);
   },
+
   async remove(file) {
     const config = strapi.plugins.upload.config;
 
     // execute delete function of the provider
     if (file.provider === config.provider) {
+      console.log("Volio delete file.mime: ", file.ext);
+      console.log("Volio delete file.path: ", file.path);
+      console.log("Volio delete file.hash: ", file.hash);
+
       await strapi.plugins.upload.provider.delete(file);
 
       const formats = file.formats;
@@ -85,6 +95,10 @@ module.exports = {
           if (shouldSkip) continue;
 
           for (const image of images) {
+            console.log("Volio delete image.mime: ", image.ext);
+            console.log("Volio delete image.path: ", image.path);
+            console.log("Volio delete image.hash: ", image.hash);
+
             promises.push(strapi.plugins.upload.provider.delete(image));
           }
         }
